b79e4852e56062b5d0418d9a2304995c8646cec6
Pranav Waghmare <piyushw55@gmail.com>
2023-03-25 17:12:15 +0530

    [Pranav]Added: Ability to create a employee payroll table in the payroll service database to manage employee payrolls

--- PayRollService.sql
+++ PayRollService.sql
@@ -6,5 +6,15 @@ CREATE DATABASE Payroll_Service
 
 USE Payroll_Service
 
+-- UC-2 Ability to create a employee payroll table in the payroll service database to manage employee payrolls
 
+CREATE TABLE employee_payroll(
+Id INT PRIMARY KEY IDENTITY(1,1),
+Name VARCHAR(30),
+Salary INT,
+StartDate DATE
+);
+
+
+SELECT * FROM employee_payroll
 
